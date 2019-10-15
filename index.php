9e0e230af793f41045942e92ffbae3c954e370af
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-15 01:43:56 -0400

    configuração das urls amigaveis.

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 
 	include "cabecalho.php"; 
 
+	$url = (isset($_GET["url"])) ? $_GET["url"]: " ";
+	$explode = explode("/", $url);
+	$paginas = array("home", "categoria", "pesquisa", "post");
 
-	include "home.php";
+	if (isset($explode[0]) && $explode[0] == " ") {
+		include "home.php";
+	}elseif ($explode[0] != " ") {
+		if (isset($explode[0]) && in_array($explode[0], $paginas)) {
+			include_once $explode[0]. ".php";
+		}else {
+			include "home.php";
+		}
+	}
 	
+	var_dump($explode);
 
 	include "rodape.php";
 	
