9c48e050b82293fd42a647fdb8e6e67530a98be8
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-15 22:21:01 -0400

    criando a comunicação com o banco e validando a tela de login.

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,12 @@
 <?php
 
-
+    //Incluindo aqui o cabecalho ja fatiado.
     include "cabecalho.php";
 
+    //Incluindo aqui o home ja fatiado.
     include "home.php";
 
+    //incluindo aqui o rodape ja fatiado.
     include "rodape.php";
 
 ?>
---
+++ admin/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/estilo.css" rel="stylesheet" type="text/css">
+
+    <!--[if lte IE 8]>
+    <script type="text/javascript">
+    var htmlshim='abbr,article,aside,audio,canvas,details,figcaption,figure,footer,header,mark,meter,nav,output,progress,section,summary,time,video'.split(',');
+    var htmlshimtotal=htmlshim.length;
+    for(var i=0;i<htmlshimtotal;i++) document.createElement(htmlshim[i]);
+    </script>
+    <![endif]-->
+    <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/abas.js"></script>
+    <title>Painel CML</title>
+</head>
+
+<body style="background:#064A5F;">
+
+<div class="base-login">
+	<div class="conteudo">
+			<div class="cx-login">
+				<img src="imagens/img-login.png">
+				<form action="" method="post">
+				<label>
+					<strong>email</strong>
+					<input type="text" name="" id="" value="">
+				 </label>
+				 <label>
+					<strong>Senha</strong>
+					<input type="password" name="" id="" value="">
+				 </label>
+				 <label>
+					<input type="submit" name="" id="" value="entrar" class="but">
+				 </label>
+				</form>
+			</div>
+	</div>
+</div>
+</body>
+</html>
