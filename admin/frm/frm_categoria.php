33074895303c5e614a409bd36d42aab0a801be35
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-28 21:05:20 -0400

    Arquivos criados para inserir categoria.

--- admin/frm/frm_categoria.php
+++ admin/frm/frm_categoria.php
@@ -2,10 +2,14 @@
 	<h1>CADASTRO DE CATEGORIAS</h1>
 		<div class="cx-form">
 		<div class="cx-pd">			
-			<form action="" method="post">	
+			<form action="op/op_categoria.php" method="post">	
 			  <label>
 				<strong>Insira uma categoria</strong>
-				<input type="text" name="txt_modulo" id="txt_modulo" value="" size="110">
+				<input type="text" name="txt_categoria"  value="" size="110">
+			  </label>
+			  <label>
+				<strong>Slug</strong>
+				<input type="text" name="txt_slug"  value="" size="110">
 			  </label>
 			  
 				<label>
---
+++ admin/op/op_categoria.php
@@ -0,0 +1,5 @@
+<?php
+
+$txt_categoria = $_POST["txt_categoria"];
+
+echo $txt_categoria;    
