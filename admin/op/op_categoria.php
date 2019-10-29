d9fc07371d0e0912b68085299f4c9c75b652ffd3
Joabe Kachorroski <kernelkill@outlook.com>
2019-10-28 21:27:09 -0400

    operação de inserção de categoria reealizada.

--- admin/op/op_categoria.php
+++ admin/op/op_categoria.php
@@ -1,5 +1,15 @@
 <?php
 
-$txt_categoria = $_POST["txt_categoria"];
+require ('../../config/config.php');
+require ('../../config/crud.php');
+require ('../../config/biblio.php');
 
-echo $txt_categoria;    
+$txt_categoria = $_POST["txt_categoria"];
+$slug = slug($txt_categoria);
+
+$dados = array(
+    "categoria" => trim($txt_categoria),
+    "slug_categoria" => $slug
+);
+
+inserir("categoria", $dados);
