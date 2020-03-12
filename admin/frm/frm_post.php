ad6db2993de55f3b7e6f8cfd17659b18826e1b51
Joabe Kachorroski <kernelkill@outlook.com>
2020-03-11 22:33:01 -0400

    Formulario do post atualizado.

--- admin/frm/frm_post.php
+++ admin/frm/frm_post.php
@@ -4,40 +4,62 @@
 	@$id	 	 = $_GET["id"];
 
 	if ($acao) {
-		$valores = consultar("categoria","id_categoria = $id");
+		$valores = consultar("post","id_post = $id");
 		//var_dump($valores);
 	}
 	
 
-	$categoria = isset($valores[0]["categoria"]) ? $valores[0]["categoria"]: NULL;
-	$slug_categoria = isset($valores[0]["slug_categoria"]) ? $valores[0]["slug_categoria"]: NULL;
+	$post = isset($valores[0]["post"]) ? $valores[0]["post"]: NULL;
+	$slug_post = isset($valores[0]["slug_post"]) ? $valores[0]["slug_post"]: NULL;
 
 ?>
 
-<div class="base-direita">	
-	<h1>CADASTRO DE CATEGORIAS</h1>
-		<div class="cx-form">
-		<div class="cx-pd">			
-			<form action="op/op_categoria.php" method="post">	
-			  <label>
-				<strong>Insira uma categoria</strong>
-				<input type="text" name="txt_categoria"  value="<?php echo $categoria ?>" size="110">
-			  </label>
-			  <label>
-				<strong>Slug</strong>
-				<input type="text" name="txt_slug"  value="<?php echo $slug_categoria ?>" size="110" disabled="disabled">
-			  </label>
-			  
-				<label>
-					<div class="cx-but">
-						<input type="hidden" name="id" value="<?php echo $id ?>">							
-						<input type="hidden" name="acao" value="<?php echo ($acao !="")? $acao: "Cadastrar" ?>">
-						<input type="button" name="cancel" value="cancel" onClick="window.location='index.php?link=2';" class="but">									
-						<input type="submit" value="<?php echo ($acao !="")? $acao: "Cadastrar" ?>" class="but">	
-					</div>
-					</label>
-			</form>
+<div class="base-direita">
+		<h1>CADASTRO DE POSTS</h1>
+<div class="cx-form">
+	<div class="cx-pd">
+	<form action="" method="post">
+<label class="esq">		
+<strong>Escolha uma categoria</strong>
+    <select name="id_modulo">
+          &nbsp;
+         <option value="1"> PHP</option><option value="2"> JAVA</option><option value="3"> CSS</option><option value="4"> JAVA</option><option value="5"> HTML</option> 
+          &nbsp;
+        </select>
+</label>
+   
+<label class="dir">
+<strong>Ativo</strong>
+	<select name="txt_ativo">
+	<option value="S">Sim</option>
+	<option value="N">Não</option>
+  </select>
+</label>
 
-			</div>
+  <label><strong>Título do post</strong>
+   <input type="text" name="txt_curso" id="txt_curso" value="" size="110">
+  </label>
+
+  <label><strong>Insira o conteudo</strong>
+   <textarea rows="8"></textarea>
+  </label>
+
+  <label><strong>Imagem do post</strong>
+   <input type="file">
+  </label>
+
+
+	
+		<label>
+		<div class="cx-but">
+			<input type="hidden" name="id" value="">
+			<input type="hidden" name="irpara" value="">							
+			<input type="hidden" name="acao" value="Inserir">										
+			<input type="submit" name="logar" id="logar" value="Inserir" class="but">	
+		</div>	
+		</label>
+</form>
+
 		</div>
-</div>
+		</div>
+	</div>
